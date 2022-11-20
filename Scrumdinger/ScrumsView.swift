d103e5598f2421b7d6f0d8f45f1f9dc7e7c07133
Dan Leedham <danleedham@googlemail.com>
2022-11-20 20:00:18 +0000

    Update to start Navigation

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -1,14 +1,12 @@
-//
-//  ScrumsView.swift
-//  Scrumdinger
-//
-//  Created by Dan Leedham on 20/11/2022.
-//
+/*
+See LICENSE folder for this sample’s licensing information.
+*/
 
 import SwiftUI
 
 struct ScrumsView: View {
     let scrums: [DailyScrum]
+    
     var body: some View {
         List {
             ForEach(scrums) { scrum in
